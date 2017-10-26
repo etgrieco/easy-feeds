061153661a01e002a20dc2e64eb5980cf09dfa86
Elliott Grieco <elliott.grieco@gmail.com>
2017-10-26 16:18:04 -0400

    Prepare for api testing

--- frontend/easy_feeds.jsx
+++ frontend/easy_feeds.jsx
@@ -4,8 +4,10 @@ import configureStore from './store/store';
 import Root from './components/root';
 
 //test
-import * as SessionApiUtil from './util/session_api_util';
-import * as SessionActions from './actions/session_actions';
+// import * as SessionApiUtil from './util/session_api_util';
+// import * as SessionActions from './actions/session_actions';
+import * as SubscriptionApiUtil from './util/subscription_api_util';
+import * as FeedApiUtil from './util/feed_api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
 
@@ -19,8 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   //test
-  window.SessionApiUtil = SessionApiUtil;
-  window.SessionActions = SessionActions;
+  window.SubscriptionApiUtil = SubscriptionApiUtil;
   window.getState = store.getState;
   window.dispatch = store.dispatch;
 
