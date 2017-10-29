4cee2f1c3518a0e4ef470c7f9273093f5cf0996e
Elliott Grieco <elliott.grieco@gmail.com>
2017-10-28 22:17:39 -0400

    Improves nabar styling

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -2,21 +2,38 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NavBar = (props) => {
-  return <div className="navbar-container">
-    <div className="navbar-collections">
-      <p>Collections will go here</p>
-      <Link to="/i/feeds">Click to Organize and Add Feeds</Link>
-      <br />
-      <Link to="/i/latest">Click to see stories</Link>
-      <div className="sample-nav-container"></div>
-      <div className="sample-nav-container"></div>
-      <div className="sample-nav-container"></div>
-      <div className="sample-nav-container"></div>
-    </div>
-    <div className="nav-add-content">
-      <Link to="/i/feeds">Add Content</Link>
-    </div>
-  </div>;
+  return (
+    <div className="navbar-container">
+      <div className="navbar-collections">
+        <div className="collections-header">
+          <div>Feeds</div>
+          <div>
+            <Link to="/i/feeds">
+              <i className="fa fa-cog" aria-hidden="true"></i>
+            </Link>
+          </div>
+        </div>
+        <div className="tabs">
+          <div className="latest-tab">
+            <Link to="/i/latest">
+              <span><i className="fa fa-bars" aria-hidden="true"></i></span>
+              All
+            </Link>
+          </div>
+        </div>
+
+      </div>
+      <div className="nav-add-content">
+        <Link to="/i/feeds">
+          <div className="nav-add-container">
+            <span><i className="fa fa-plus" aria-hidden="true"></i></span>
+            Add Content
+          </div>
+        </Link>
+      </div>
+  </div>
+  );
+
 };
 
 export default NavBar;
