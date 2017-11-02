41501e697c4b8a5523e2fbb2c285f3f0e1beadd4
Elliott Grieco <elliott.grieco@gmail.com>
2017-11-02 15:07:08 -0400

    Implements Hide/show navbar

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -3,6 +3,11 @@ import { Link } from 'react-router-dom';
 
 class NavBar extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { hidden: false };
+  }
+
   componentDidMount() {
     // change to fetch all collections!!
       // -- should include collections + basic subscription info
@@ -14,6 +19,7 @@ class NavBar extends React.Component {
     const feed = this.props.feeds[feedId];
       return (
         <li key={feedId}>
+          <img src={feed.favicon_url} />
           <Link to={`/i/subscriptions/${feed.id}`}>
             {feed.subscription_title}
           </Link>
@@ -22,39 +28,46 @@ class NavBar extends React.Component {
     });
 
     return (
-      <div className="navbar-container">
-        <div className="navbar-collections">
-          <div className="collections-header">
-            <div>Feeds</div>
-            <div>
-              <Link to="/i/feeds/">
-                <i className="fa fa-cog" aria-hidden="true"></i>
-              </Link>
+      <aside className="navbar-container">
+        <div className={`navbar-closed ${this.state.hidden ? "" : "hidden"}`}>
+          <span className="navbar-show-button" onClick={e => this.setState({hidden: false})}>
+            <i className="fa fa-bars" aria-hidden="true"></i>
+          </span>
+        </div>
+        <div className={`navbar-open ${this.state.hidden ? "hidden" : ""}`}>
+          <div onClick={e => this.setState({hidden: true})}>Hide</div>
+          <div className="navbar-collections">
+            <div className="feeds-header noselect">
+              <div>Feeds</div>
+              <div>
+                <Link to="/i/feeds/">
+                  <i className="fa fa-cog" aria-hidden="true"></i>
+                </Link>
+              </div>
+            </div>
+            <div className="tabs-container">
+              <div className="latest">
+                <Link to="/i/latest">
+                  <span><i className="fa fa-bars" aria-hidden="true"></i></span>
+                  Latest
+                </Link>
+              </div>
+
+              <div className="feeds">
+                {feedsList}
+              </div>
             </div>
           </div>
-          <div className="tabs">
-            <div className="latest-tab">
-              <Link to="/i/latest">
-                <span><i className="fa fa-bars" aria-hidden="true"></i></span>
-                All
-              </Link>
-            </div>
-
-            <div>
-              {feedsList}
-            </div>
+          <div className="nav-add-content">
+            <Link to="/i/discover">
+              <div className="nav-add-container">
+                <span><i className="fa fa-plus" aria-hidden="true"></i></span>
+                Add Content
+              </div>
+            </Link>
           </div>
-
         </div>
-        <div className="nav-add-content">
-          <Link to="/i/discover">
-            <div className="nav-add-container">
-              <span><i className="fa fa-plus" aria-hidden="true"></i></span>
-              Add Content
-            </div>
-          </Link>
-        </div>
-    </div>
+      </aside>
     );
   }
 }
