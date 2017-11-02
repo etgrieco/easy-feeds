dd9aca7ddfbfdf82efc71b2ac7e1cd9d594821c1
Elliott Grieco <elliott.grieco@gmail.com>
2017-11-02 17:15:16 -0400

    Implements styling on most of navbar

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -18,56 +18,57 @@ class NavBar extends React.Component {
     const feedsList = this.props.feedIds.map(feedId => {
     const feed = this.props.feeds[feedId];
       return (
+        <Link to={`/i/subscriptions/${feed.id}`}>
         <li key={feedId}>
           <img src={feed.favicon_url} />
-          <Link to={`/i/subscriptions/${feed.id}`}>
             {feed.subscription_title}
-          </Link>
         </li>
+      </Link>
       );
     });
 
     return (
-      <nav className={`navbar-container${this.state.hidden ? "-hidden" : ""} navbar-transition`}>
-        <div className={`navbar-closed${this.state.hidden ? "":  "-hidden"} contents-transition`}>
-          <span className="navbar-show-button" onClick={e => this.setState({hidden: false})}>
-            <i className="fa fa-bars" aria-hidden="true"></i>
-          </span>
-        </div>
-        <div className={`navbar-contents${this.state.hidden ? "-hidden" : ""} contents-transition`}>
-          <span onClick={e => this.setState({hidden: true})}>Hide</span>
-          <div className="navbar-collections">
-            <div className="feeds-header noselect">
-              <div>Feeds</div>
-              <div>
-                <Link to="/i/feeds/">
-                  <i className="fa fa-cog" aria-hidden="true"></i>
-                </Link>
+      <section className="navbar">
+        <nav className={`navbar-container${this.state.hidden ? "-hidden" : ""} navbar-transition`}>
+          <div className={`navbar-closed${this.state.hidden ? "":  "-hidden"}`}>
+            <span className="navbar-show-button" onClick={e => this.setState({hidden: false})}>
+              <i className="fa fa-bars" aria-hidden="true"></i>
+            </span>
+          </div>
+          <div className={`navbar-contents${this.state.hidden ? "-hidden" : ""}`}>
+            <span onClick={e => this.setState({hidden: true})}>Hide</span>
+            <div className="navbar-collections">
+              <div className="feeds-header noselect">
+                <div>Feeds</div>
+                <div>
+                  <Link to="/i/feeds/">
+                    <i className="fa fa-cog" aria-hidden="true"></i>
+                  </Link>
+                </div>
               </div>
-            </div>
-            <div className="tabs-container">
-              <div className="latest">
-                <Link to="/i/latest">
-                  <span><i className="fa fa-bars" aria-hidden="true"></i></span>
-                  Latest
+              <div className="tabs-container">
+                <div className="feeds">
+                  <Link to="/i/latest" className="latest">
+                  <li>
+                      <span><i className="fa fa-bars" aria-hidden="true"></i></span>
+                      Latest
+                  </li>
                 </Link>
-              </div>
-
-              <div className="feeds">
-                {feedsList}
+                  {feedsList}
+                </div>
               </div>
             </div>
           </div>
-          <div className="nav-add-content">
-            <Link to="/i/discover">
-              <div className="nav-add-container">
-                <span><i className="fa fa-plus" aria-hidden="true"></i></span>
-                Add Content
-              </div>
-            </Link>
-          </div>
-        </div>
-      </nav>
+        </nav>
+        <aside className="nav-add-content-container">
+          <Link to="/i/discover">
+            <div className="nav-add-content">
+              <span><i className="fa fa-plus" aria-hidden="true"></i></span>
+              Add Content
+            </div>
+          </Link>
+        </aside>
+      </section>
     );
   }
 }
