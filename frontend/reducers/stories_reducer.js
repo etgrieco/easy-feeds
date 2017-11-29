22547d2f4a5b002b560bf3a2e2bce6be6de4cdf6
Elliott Grieco <elliott.grieco@gmail.com>
2017-11-29 18:25:42 -0500

    Adds read/unread to reducers

--- frontend/reducers/stories_reducer.js
+++ frontend/reducers/stories_reducer.js
@@ -1,7 +1,7 @@
 import merge from 'lodash/merge';
 import { combineReducers } from 'redux';
 import { RECEIVE_SINGLE_FEED, RECEIVE_NEW_FEED } from '../actions/subscription_actions';
-import { RECEIVE_LATEST, RECEIVE_STORY, RECEIVE_READS } from '../actions/story_actions';
+import { RECEIVE_LATEST, RECEIVE_STORY, RECEIVE_READS, RECEIVE_READ, RECEIVE_UNREAD } from '../actions/story_actions';
 import { CLEAR_ENTITIES } from '../actions/session_actions';
 
 const storiesById = (state = {}, action) => {
@@ -16,6 +16,14 @@ const storiesById = (state = {}, action) => {
     case RECEIVE_STORY:
     case RECEIVE_READS:
       newState = merge({}, state, action.stories.byId);
+      return newState;
+    case RECEIVE_READ:
+      newState = merge({}, state);
+      newState[action.stories.allIds[0]].read = true;
+      return newState;
+    case RECEIVE_UNREAD:
+      newState = merge({}, state);
+      newState[action.stories.allIds[0]].read = false;
       return newState;
     case CLEAR_ENTITIES:
       return {};
