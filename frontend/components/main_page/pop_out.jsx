7ecf5c7ddf77c4655f260cfc5835a88ece304980
Elliott Grieco <elliott.grieco@gmail.com>
2017-12-09 20:45:59 -0500

    Handles esc keypress to exit popout

--- frontend/components/main_page/pop_out.jsx
+++ frontend/components/main_page/pop_out.jsx
@@ -1,26 +1,44 @@
 import React from 'react';
 
-export default function PopOut({component: Component, closePopOut}) {
+export default class PopOut extends React.Component {
 
-  const handleClose = (e) => {
-    if(e.target.className === "pop-out-modal-screen" ||
-       e.target.parentElement.className.includes("pop-out-exit")
-      ) { closePopOut(); }
+  handleEscKey = (event) => {
+    if(event.keyCode === 27){
+      this.props.closePopOut();
+    }
   }
 
-  return(
-    <div className="pop-out-modal-screen"
-         onClick={e => handleClose(e)}>
-      <div className="pop-out-exit noselect">
-        <div className="noselect" onClick={e => handleClose(e)}>
-          &#10006;
+  componentWillMount() {
+    document.addEventListener("keydown", this.handleEscKey, false);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleEscKey, false);
+  }
+
+  handleClose = e => {
+    if(e.target.className === "pop-out-modal-screen" ||
+       e.target.className.includes("close-popout")
+     ) { this.props.closePopOut(); }
+  }
+
+  render() {
+    const { component: Component, closePopOut } = this.props;
+
+    return (
+      <div className="pop-out-modal-screen"
+           onClick={e => this.handleClose(e)}>
+        <div className="pop-out-exit noselect">
+          <div className="close-popout noselect" onClick={e => this.handleClose(e)}>
+            &#10006;
+          </div>
+        </div>
+
+        <div className="pop-out-window">
+          <Component />
         </div>
       </div>
-
-      <div className="pop-out-window">
-        <Component />
-      </div>
-    </div>
-  );
+    );
+  }
 
 }
