b2d61efb47a88a55d4e963949f8165bb18dc99df
Elliott Grieco <elliott.grieco@gmail.com>
2017-12-21 14:54:40 -0500

    Refactors navbar into separte components; simplifies some hidden logic

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class NavBar extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { hidden: false };
-  }
+  state = { isOpen: true };
 
   componentDidMount() {
     this.props.fetchAllSubscriptions();
   }
 
+  handleClick = () => {
+    this.setState(({ isOpen }) => ({isOpen: !isOpen}));
+  }
+
   render() {
     const feedsList = this.props.feedIds.map(feedId => {
-    const feed = this.props.feeds[feedId];
+      const feed = this.props.feeds[feedId];
       return (
         <Link key={feedId} to={`/i/subscriptions/${feed.id}`}>
           <li>
@@ -25,74 +25,86 @@ class NavBar extends React.Component {
     });
 
     return (
-      <section className={`navbar${this.state.hidden ? " navbar-closed" : ""}`}>
-        <div className="menu-container">
-          <div>
-            <Link to="/i/feeds/">
-              <div className={`edit-button${this.state.hidden ? " hidden" : ""}`}>Organize Feeds<i className="fa fa-cog" aria-hidden="true"></i></div>
-            </Link>
-          </div>
-          <div className={`navbar-show-button${this.state.hidden ? "-hidden" : ""}`}>
-            <span onClick={e => this.setState({hidden: !this.state.hidden})}>
-              { this.state.hidden ?
-                <i className="fa fa-expand" aria-hidden="true"></i> :
-                  <i className="fa fa-compress" aria-hidden="true"></i>
-                }
-              </span>
-          </div>
-        </div>
-        <NavBarMenu hidden={this.state.hidden} feedsList={feedsList} />
-        <NavBarAddContent hidden={this.state.hidden} />
+      <section className="navbar">
+        <NavBarMenu {...this.state} handleClick={this.handleClick} />
+        <NavBarLinks {...this.state} feedsList={feedsList} />
+        <NavBarAddContent {...this.state} />
       </section>
     );
   }
 }
 
-const NavBarMenu = ({ hidden, feedsList }) => (
-  <nav className={`navbar-container${hidden ? "-hidden" : ""} navbar-transition`}>
-    <div className={`navbar-closed${hidden ? "":  "-hidden"}`}>
-    </div>
-    <div className={`navbar-contents${hidden ? "-hidden" : ""}`}>
-      <div className="navbar-collections">
-        <div className="feeds-header noselect"></div>
-        <div className="tabs-container">
+const NavBarMenu = (props) => (
+  <div className="menu-container">
+    { props.isOpen ?
+      <div>
+        <Link to="/i/feeds/">
+          <div className="edit-button">Organize Feeds
+            <i className="fa fa-cog" aria-hidden="true"></i>
+          </div>
+        </Link>
+      </div>
+      : null
+    }
+    <NavBarCollapseExpand {...props} />
+  </div>
+);
+
+const NavBarCollapseExpand = ({ isOpen, handleClick }) => (
+  <div className="navbar-show-button">
+    <span onClick={handleClick}>
+      {isOpen ?
+        <i className="fa fa-expand" aria-hidden="true"></i>
+        : <i className="fa fa-compress" aria-hidden="true"></i>
+      }
+      </span>
+  </div>
+);
+
+const NavBarLinks = ({ isOpen, feedsList }) => (
+  isOpen ?
+    <nav className="navbar-container">
+      <div className="navbar-contents">
+        <div className="navbar-collections">
+          <div className="feeds-header noselect"></div>
+          <div className="tabs-container">
             <div className="feeds">
               <div className="special-feeds">
-              <Link to="/i/latest" className="latest">
-                <li><span><i className="fa fa-bars" aria-hidden="true"></i></span>
-                Latest
-                </li>
-              </Link>
+                <Link to="/i/latest" className="latest">
+                  <li><span><i className="fa fa-bars" aria-hidden="true"></i></span>
+                    Latest
+                  </li>
+                </Link>
 
-              <Link to="/i/reads" className="reads">
-                <li><span><i className="fa fa-book" aria-hidden="true"></i></span>
-                Recently Read
-              </li>
-              </Link>
-            </div>
-            <div className="feeds-list">
-              {feedsList}
+                <Link to="/i/reads" className="reads">
+                  <li>
+                    <span>
+                      <i className="fa fa-book" aria-hidden="true"></i>
+                    </span>
+                    Recently Read
+                  </li>
+                </Link>
+              </div>
+              <div className="feeds-list">
+                {feedsList}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+    : null
 );
 
-function NavBarAddContent(props) {
-  const { hidden } = props;
-
-  return (
-    <aside className={`nav-add-content-container ${hidden ? "hidden" : ""}`}>
-      <Link to="/i/discover">
-        <div className="nav-add-content">
-          <span className="nav-add-content-plus"><i className="fa fa-plus" aria-hidden="true"></i></span>
-          Add Content
-        </div>
-      </Link>
-    </aside>
-  );
-}
+const NavBarAddContent = ({ isOpen }) => (
+  <aside className={`nav-add-content-container ${isOpen ? "hidden" : ""}`}>
+    <Link to="/i/discover">
+      <div className="nav-add-content">
+        <span className="nav-add-content-plus"><i className="fa fa-plus" aria-hidden="true"></i></span>
+        Add Content
+      </div>
+    </Link>
+  </aside>
+);
 
 export default NavBar;
