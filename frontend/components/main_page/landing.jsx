568152b4c5392a321614a49d18a54bbf2d45d26a
Elliott Grieco <elliott.grieco@gmail.com>
2018-01-24 12:59:53 -0500

    Restore original landing pages

--- frontend/components/main_page/landing.jsx
+++ frontend/components/main_page/landing.jsx
@@ -7,37 +7,26 @@ import { createDemoUser } from '../../actions/session_actions';
 function Landing(props) {
   return (
     <div className="landing">
-      <div className="info-box background-grey">
+      <div>
         <section className="landing-welcome">
           <h1>Welcome to EasyFeeds</h1>
           <p>An easy way to aggregate stories from across the web.</p>
-            <button
-              className="green-button"
-              onClick={e => props.history.push("/signup")}>
-              Get Started</button>
-            <button
-              className="green-button demo-user"
-              onClick={e => props.createDemoUser()
-              }>
-              Demo User
-            </button>
+          <button
+            className="green-button"
+            onClick={e => props.history.push("/signup")}>
+            Get Started</button>
+          <button
+            className="green-button demo-user"
+            onClick={e => props.createDemoUser()
+            }>
+            Demo User</button>
         </section>
         <div className="landing-image-container">
-          <img src="https://i.imgur.com/Exbov3T.png" />
+          <img src="https://i.imgur.com/jW9WDRh.png" />
         </div>
+        <Route path="/login" component={SessionFormContainer} />
+        <Route path="/signup" component={SessionFormContainer} />
       </div>
-      <div className="info-box background-white">
-        This is a test
-      </div>
-      <div className="info-box background-grey">
-        This is a test
-      </div>
-      <div className="info-box background-white">
-        This is a test
-      </div>
-
-      <Route path="/login" component={SessionFormContainer} />
-      <Route path="/signup" component={SessionFormContainer} />
     </div>
   );
 };
