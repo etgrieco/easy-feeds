9191e57426c8bd9c54062ddc56fe87094bb37132
Elliott Grieco <elliott.grieco@gmail.com>
2018-02-14 17:17:36 -0500

    Fixes syntax error

--- app/controllers/api/subscriptions_controller.rb
+++ app/controllers/api/subscriptions_controller.rb
@@ -21,8 +21,8 @@ class Api::SubscriptionsController < ApplicationController
   def update
     # note: takes a subscription id vs. a feed id
     @subscription = current_user.subscriptions.find_by(id: params[:id])
-
-    elsif @subscription.update(subscription_params)
+    # check for empty string? Ok in model?
+    if @subscription.update(subscription_params)
       render 'api/subscriptions/show_no_stories'
     else
       render json: @subscription.errors.full_messages, status: 422
