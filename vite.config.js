0d1ab1e0115573b8e1fafe97fce33e96582eff2f
etgrieco <elliott.grieco@gmail.com>
2024-03-23 20:21:17 -0400

    Fix local env + ensure inclusion of vite built assets

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/assets/dist',
+  base: process.env.NODE_ENV === 'production' ?  '/assets/dist' : undefined,
   build: {
     manifest: true
   },
