6030667af5979cb11fc75ec8de7f4f0fea4af760
etgrieco <elliott.grieco@gmail.com>
2024-03-23 20:14:03 -0400

    Add dynamic manifest-based asset loading

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
+require 'json'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?, :login
+  helper_method :current_user, :logged_in?, :login, :asset_manifest
 
   def current_user
     @current_user ||= User.find_by(session_token: session[:session_token])
@@ -25,6 +27,14 @@ class ApplicationController < ActionController::Base
     unless logged_in?
       redirect_to "/"
     end
+  end
+
+  def asset_manifest
+    manifest_file = File.read("./app/assets/static/dist/.vite/manifest.json")
+    manifest_dict = JSON.parse(manifest_file)["index.html"]
+    entry_js = manifest_dict["file"]
+    css_files = manifest_dict["css"]
+    { :entry_js => entry_js, :css_files => css_files }
   end
 
 end
